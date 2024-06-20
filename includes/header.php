83bf520b34d66e6efba22af27d954492db00cd84
oscar05 <santiagoM0509@hotmail.com>
2024-06-20 18:21:50 -0500

    Primer commit

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<header>
+    <nav id="navbar">
+        <div class="container-logo-navbar">
+            <img class="logo-navbar" src="assets/images/logoProyecto.png" alt="logo">
+            <p>Autumn</p>
+        </div>
+        <div class="navbar-left">
+            <ul>
+                <li>
+                    Workspace
+                    <img class="chevron-down-navbar" src="assets/images/chevron-down-solid.svg" alt="workspaces">
+                </li>
+                <li>
+                    <button class="button_create_navbar">Create</button>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-right">
+            <ul>
+                <li>
+                    <div class="search-navbar">
+                        <img class="search-image_navbar" src="assets/images/search-img.svg" alt="search">
+                        <p>Search</p>
+                    </div>
+                </li>
+                <li>
+                    <img class="notification_navbar" src="assets/images/notification.svg" alt="notification">
+                </li>
+                <li>
+                    <img class="info_navbar" src="assets/images/info.svg" alt="info">
+                </li>
+                <li>
+                    <div>
+                        <span>OM</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('config/config.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestión de Proyectos - Inicio</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php include('includes/header.php'); ?>
+    <!-- ?php include('includes/aside.php'); ?> -->
+    <main>
+        <!-- Contenido principal de la página de inicio -->
+    </main>
+    <!-- ?php include('includes/footer.php'); ?> -->
+    <script src="js/main.js"></script>
+</body>
+</html>
