a85494d5cfd50585d029c1694df6f6edd58b083b
oscar05 <santiagoM0509@hotmail.com>
2024-12-03 17:24:55 -0500

    Successful database connection setup

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,15 @@
-require('dotenv');
+require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   apiKey: process.env.API_KEY,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbUrl: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
   smtpEmail: process.env.SMTP_EMAIL,
   smtpPass: process.env.SMTP_PASS
---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+// const { checkApiKey } = require('./middlewares/auth.handler');
+// const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+
+// const routerApi = require('./routes');
+const { config } = require('./config/config');
+
+const port = config.port || 3000;
+const app = express();
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin) || !origin){
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido"));
+    }
+  }
+}
+app.use(cors(options));
+
+// require('./utils/auth');
+
+// routerApi(app);
+
+// app.use(logErrors);
+// app.use(ormErrorHandler);
+// app.use(boomErrorHandler);
+// app.use(errorHandler);
+
+app.listen(port);
