6336f8de4baf69f219c8cfee67eee537294b8fbd
oscar05 <santiagoM0509@hotmail.com>
2024-12-04 08:12:28 -0500

    card model created

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,7 +1,11 @@
 const { User, UserSchema } = require('./user.model');
+const { List, ListSchema } = require('./list.model');
+const { Card, CardSchema } = require('./card.model');
 
 function setupModels(){
   User.init(UserSchema, User.config(sequelize));
+  List.init(ListSchema, List.config(sequelize));
+  Card.init(CardSchema, List.config(sequelize));
 
   // User.associate(sequelize.models);
 }
