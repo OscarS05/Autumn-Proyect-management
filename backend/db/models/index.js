ab23b42580617c55fb99357ef9d8fc376ae6764d
oscar05 <santiagoM0509@hotmail.com>
2024-12-04 07:39:24 -0500

    User model created

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -1,5 +1,9 @@
-function setupModels(){
+const { User, UserSchema } = require('./user.model');
 
+function setupModels(){
+  User.init(UserSchema, User.config(sequelize));
+
+  // User.associate(sequelize.models);
 }
 
 module.exports = setupModels;
