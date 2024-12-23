08fcf47e426bb5560acb2b6f66ac54c9d520bbbd
oscar05 <santiagoM0509@hotmail.com>
2024-12-23 14:03:22 -0500

    Trying fig bug again

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,11 @@ require('./utils/cron');
 
 routerApi(app);
 
-app.use(express.static(path.join(__dirname, 'public')));
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.use(logErrors);
